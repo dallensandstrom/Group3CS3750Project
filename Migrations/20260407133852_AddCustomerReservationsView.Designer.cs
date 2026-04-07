82473a7ff29209fa1d5b11796d2765a77e5ab1a1
Dylan Stout <dylan.stout1204@gmail.com>
2026-04-07 08:30:53 -0600

    Added filtering for reservation list, guest view that can do basic crud for only their own reservations, change the ability to delete reservations to cancel, required status field to only be canceled, confirmed, or pending.

--- Migrations/20260407133852_AddCustomerReservationsView.Designer.cs
+++ Migrations/20260407133852_AddCustomerReservationsView.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GroupThreeTrailerParkProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260322164443_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20260407133852_AddCustomerReservationsView")]
+    partial class AddCustomerReservationsView
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
